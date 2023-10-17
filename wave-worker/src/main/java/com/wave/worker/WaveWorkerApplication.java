c268ff9ef90501ae2368661c1e828babb22fd6ab
JulioFreitas <juliosparapan2014@gmail.com>
2023-10-17 16:19:56 -0300

    Project wave-worker created

---
+++ wave-worker/src/main/java/com/wave/worker/WaveWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.wave.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveWorkerApplication.class, args);
+	}
+
+}
---
+++ wave-worker/src/test/java/com/wave/worker/WaveWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
