6352c4e27c4be24e2e82b18a7ab89c051f2508da
JulioFreitas <juliosparapan2014@gmail.com>
2023-10-18 11:00:24 -0300

    Project wave-payroll created

---
+++ wave-payroll/src/test/java/com/wave/payroll/WavePayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavePayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
