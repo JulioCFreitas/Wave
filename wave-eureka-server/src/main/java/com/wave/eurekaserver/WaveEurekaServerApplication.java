b7610de9f18f76ff35df5a56e7876d531db2b8f3
JulioFreitas <juliosparapan2014@gmail.com>
2023-10-24 16:16:54 -0300

    Project wave-eureka-server created

---
+++ wave-eureka-server/src/main/java/com/wave/eurekaserver/WaveEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.wave.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveEurekaServerApplication.class, args);
+	}
+
+}
