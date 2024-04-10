6ef51d74018bd9d76e0340d7a02f5d22afab5ccc
JulioFreitas <juliosparapan2014@gmail.com>
2024-04-10 15:56:17 -0300

    Hystrix fault tolerance

---
+++ wave-api-gateway-zuul/src/main/java/com/wave/apigatewayzuul/WaveApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.wave.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ wave-api-gateway-zuul/src/test/java/com/wave/apigatewayzuul/WaveApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
