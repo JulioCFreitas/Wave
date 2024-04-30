41da2247020b45673a753503c9bc0bab0c4fa4d8
JulioFreitas <juliosparapan2014@gmail.com>
2024-04-30 14:59:15 -0300

    Project wavw-config-server created

---
+++ wave-config-server/src/main/java/com/wave/configserver/WaveConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.wave.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveConfigServerApplication.class, args);
+	}
+
+}
---
+++ wave-config-server/src/test/java/com/wave/configserver/WaveConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
